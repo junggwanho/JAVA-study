27f63568445a259eb39cceda15ea6662491a00d6
junggwanho <rhksgh1222@naver.com>
2023-06-29 15:28:21 +0900

    java 학습

--- ch03/S05.java
+++ ch03/S05.java
@@ -10,6 +10,10 @@ public class S05 {
         String man = age > 65 ? "할아버지" : "아저씨";
 
         System.out.println(man);
+
+        String man1 = age > 65 ? "할아버지" : (age > 20 ? "아저씨" : "학생");
+        
+        System.out.println(man1);
     }
     
 }
